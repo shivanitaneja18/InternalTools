7007bdf74eae32d1880e4d840bacd1177e03da8a
shivanitaneja18 <shivani.taneja104@gmail.com>
2017-10-16 02:06:41 +0530

    Commit

---
+++ loginjs/controller.js
@@ -0,0 +1,30 @@
+app.controller("myctrl",($scope, $window ,myfactory)=>{
+   
+    $scope.submitform = ()=>{
+    
+        var obj = {};
+        
+         
+        obj.username= "c" + $scope.mobilenumber;
+        obj.password=$scope.password;
+        
+        console.log(obj)
+       
+          var promise = myfactory.callserver(obj);
+          promise.then((data)=>{
+                 
+                console.log(data);
+
+                if (data.key) {
+                    
+                     
+                    $window.location.href = "https://www.halanx.com/tools/index.html";
+
+                }
+            },(err)=>{
+                
+                alert("error");
+            })
+       
+    }
+})
